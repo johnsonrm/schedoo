d8c2cdadc4efb2f40325929df234a7e4a40ac5f0
Rob Johnson <majesticgeek@outlook.com>
2023-09-04 15:54:52 -0400

    initial firebase config

---
+++ src/app/services/routine-schedule.service.ts
@@ -0,0 +1,34 @@
+
+
+require('dotenv').config({path: 'src/environments/.env'});
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID //optional after firebase 7.20.0
+};
+
+
+
+export class RoutineScheduleService {
+
+  constructor() {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+   }
+
+
+
+}
