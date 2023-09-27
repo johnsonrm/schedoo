bd7cd496aff4e526dde02462088868b10206a5ac
Rob Johnson <majesticgeek@outlook.com>
2023-09-27 17:16:54 -0400

    create login page with improved UI handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-// import { Firestore, collection } from '@angular/fire/firestore';
-// import { OnInit } from '@angular/core';
+import { User } from "firebase/auth";
+import { Select } from '@ngxs/store';
+import { UserStateModel } from './store/states/user.state';
+import { Observable } from 'rxjs';
+import { FirebaseService } from './services/firebase.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,6 +15,27 @@ import { HeaderComponent } from './header/header.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'monthly';
+
+  public signedIn: boolean = false;
+
+  @Select((state: {user: UserStateModel}) => state.user.userData) userData$ : Observable<User>;
+
+  constructor(private firebaseService: FirebaseService) {
+
+    this.userData$.subscribe((userData: User) => {
+      this.signedIn = userData?.uid ? true : false;
+    });
+
+   }
+
+  signIn() {
+    this.firebaseService.signIn();
+  }
+
+  signOut() {
+    this.firebaseService.signOut();
+  }
 
 }
